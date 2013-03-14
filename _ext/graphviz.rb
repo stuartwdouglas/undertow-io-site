87101fe6e3b06acc56b3045ebeedaf1dea62be6b
Stuart Douglas <stuart.w.douglas@gmail.com>
2013-03-15 08:41:16 +1100

    Minor fixes

--- _ext/graphviz.rb
+++ _ext/graphviz.rb
@@ -1,13 +1,9 @@
-require 'pp'
-
 class Graphviz
   def transform(site, page, input)
     ext = File.extname(page.output_path)[1..-1].to_sym
     case ext
       when :dot
-        command = "dot -Tpng ." + page.url + " >"  + site.output_dir + page.output_path[0..(page.output_path.length-5)] + ".png"
-        pp command
-        system(command)
+        system("dot -Tpng ." + page.url + " >"  + site.output_dir + page.output_path[0..(page.output_path.length-5)] + ".png")
     end
     return input
   end
